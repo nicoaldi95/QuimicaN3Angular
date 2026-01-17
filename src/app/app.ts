e21022343db97f7be4cf5281484e549985109036
nadiazdi13 <nicoaldi95@gmail.com>
2026-01-17 14:56:44 -0500

    Perfil

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PeriodicTable } from "./features/periodic-table/periodic-table";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PeriodicTable],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
