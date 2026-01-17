1e4bf81f06be6a007e5bc7f1ff5eff29098d550b
nadiazdi13 <nicoaldi95@gmail.com>
2026-01-17 16:25:18 -0500

    rutas

--- src/app/features/balancer/balancer.ts
+++ src/app/features/balancer/balancer.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-balancer',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './balancer.html',
   styleUrl: './balancer.css',
 })
